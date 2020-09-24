bedc265b70308b0fa5ebcfef8da33a4020b0491f
TatjaZ <t.zenkovich1974@gmail.com>
2020-09-24 17:26:23 +0300

    Функции
    Signed-off-by: TatjaZ <t.zenkovich1974@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-print ("hello world!")
-print ("hello world2")
-for i in range(10):
-    print(i)
+# print ("hello world!")
+# print ("hello world2")
+# for i in range(10):
+#     print(i)
 # git commcd.ent
+# 1 point
+a = int(input())
+b = int(input())
+c = int(input())
+#print(a and b and c and "Нет нулевых значений!!!")
+print(a or b or c or "Введены все нули!")
