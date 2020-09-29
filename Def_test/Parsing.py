6eec6e909f9214c00b72cc8fe5e512e0331f0631
TatjaZ <t.zenkovich1974@gmail.com>
2020-09-29 16:04:36 +0300

    Generator
    Signed-off-by: TatjaZ <t.zenkovich1974@gmail.com>

--- Def_test/Parsing.py
+++ Def_test/Parsing.py
@@ -1,3 +1,5 @@
+from typing import List, Any
+
 fp = open('access.log', 'r')
 lines = fp.readlines()
 fp.close()
@@ -7,17 +9,17 @@ def parse_line(line):
     rest, user_agent =  line[:-6].rsplit(maxsplit=1)
     return {'ip': ip, 'user_agent': user_agent}
 parse_line(lines[0])
-    requests = []
-for line in lines:
-    r = parse_line(line)
-    requests.append(r)
-ips = [request['ip'] for request in requests]
-print('Количество уникальных ip: ' + str(len(set(ips))))
-user_agents = [request['user_agent'] for request in requests]
-print('Количество браузеров: ' + str(len(set(user_agents))))
-print('Список браузеров: ' + '\n'.join(set(user_agents)))
-ua_count = {user_agent: 0 for user_agent in user_agents}
-for request in requests:
-    r_user_agent = request["user_agent"]
-    ua_count[r_user_agent] += 1
-ua_count
+# Requests = [ ]
+# for line in lines:
+#     r = parse_line(line)
+#     requests.append(r)
+# ips = [request['ip'] for request in requests]
+# print('Количество уникальных ip: ' + str(len(set(ips))))
+# user_agents = [request['user_agent'] for request in requests]
+# print('Количество браузеров: ' + str(len(set(user_agents))))
+# print('Список браузеров: ' + '\n'.join(set(user_agents)))
+# ua_count = {user_agent: 0 for user_agent in user_agents}
+# for request in requests:
+#     r_user_agent = request["user_agent"]
+#     ua_count[r_user_agent] += 1
+# ua_count
